ba1c4ae7fd9820f68d4485d353986e4a6a706077
Lilian Jónsdóttir <lilian.jonsdottir@gmail.com>
2020-08-04 21:13:18 -0700

    version bump

--- MWSE/mods/celediel/DoorRandomizer/common.lua
+++ MWSE/mods/celediel/DoorRandomizer/common.lua
@@ -2,7 +2,7 @@ local this = {}
 
 this.modName = "Door Randomizer" -- or something
 this.author = "Celediel"
-this.version = "1.0.1"
+this.version = "1.1.0"
 this.modInfo = "When a cell-change door is activated, its destination is randomized, based on various configuration options"
 this.configPath = string.gsub(this.modName, "%s+", "")
 this.cellTypes = {
